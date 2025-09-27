8712c80e7e756f3fa51f9893bbef40cac9306e32
patelvedant240307-lang <patelvedant240307@gmail.com>
2025-09-27 19:12:22 +0530

    Create Calculate net salary where: Net Salary = Gross Salary + Allowances – Deductions Conditions: • If Gross Salary > 10000: Allowance = 10%, Deduction = 3% • If Gross Salary > 5000: Allowance = 7%, Deduction = 2%.c

---
+++ Calculate net salary where: Net Salary = Gross Salary + Allowances – Deductions Conditions: • If Gross Salary > 10000: Allowance = 10%, Deduction = 3% • If Gross Salary > 5000: Allowance = 7%, Deduction = 2%.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    float gross, allowance, deduction, net;
+
+    printf("Enter Gross Salary: ");
+    scanf("%f", &gross);
+
+    
+    if (gross > 10000) {
+        allowance = 0.10 * gross;  
+        deduction = 0.03 * gross;  
+    } 
+    else if (gross > 5000) {
+        allowance = 0.07 * gross;  
+        deduction = 0.02 * gross;  
+    } 
+    else {
+        allowance = 0;  
+        deduction = 0;  
+    }
+
+    
+    net = gross + allowance - deduction;
+
+    printf("Net Salary = %.2f\n", net);
+
+    return 0;
+}
