fc10ab528c05a52831067041b5b1aa036c9ba8b3
patelvedant240307-lang <patelvedant240307@gmail.com>
2025-09-28 11:38:44 +0530

    Create First n even numbers.c

---
+++ First n even numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("First %d even numbers are:\n", n);
+    for(i = 1; i <= n; i++) {
+        printf("%d\n", 2 * i);
+    }
+
+    return 0;
+}
